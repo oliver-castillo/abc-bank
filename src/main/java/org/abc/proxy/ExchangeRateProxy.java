cd805c96cbb6c9133dc6671876f7d389c0a79028
Oliver Castillo <olcastillot@gmail.com>
2025-08-17 23:05:43 -0500

    feat: update ExchangeRateProxy to use LocalDate and improve property names

--- src/main/java/org/abc/proxy/ExchangeRateProxy.java
+++ src/main/java/org/abc/proxy/ExchangeRateProxy.java
@@ -1,19 +1,24 @@
 package org.abc.proxy;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class ExchangeRateProxy {
-    private Date fecha;
-    private Double sunat;
-    private Double compra;
-    private Double venta;
+    @JsonProperty("fecha")
+    private LocalDate date;
+    @JsonProperty("sunat")
+    private Double sunatExchangeRate;
+    @JsonProperty("compra")
+    private Double buyingExchangeRate;
+    @JsonProperty("venta")
+    private Double sellingExchangeRate;
 }
