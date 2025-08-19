e4dd88abb65bd4870ce8f4844ffff80efff21fbb
Oliver Castillo <olcastillot@gmail.com>
2025-08-19 18:10:17 -0500

    feat: add validation for DNI parameter in exchange rate endpoints

--- src/main/java/org/abc/resource/ExchangeRateResource.java
+++ src/main/java/org/abc/resource/ExchangeRateResource.java
@@ -1,5 +1,7 @@
 package org.abc.resource;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
@@ -8,11 +10,20 @@ import org.abc.model.dto.RequestDto;
 
 @Path("/exchange-rate")
 public interface ExchangeRateResource {
+
     @GET
     @Path("/info")
-    ExchangeRateDto getExchangeRate(@QueryParam("dni") String dni);
+    ExchangeRateDto getExchangeRate(
+            @QueryParam("dni")
+            @NotNull(message = "El número de DNI es requerido")
+            @Pattern(regexp = "^\\d{8}$", message = "El número de DNI debe contener 8 dígitos numéricos")
+            String dni);
 
     @GET
     @Path("/requests-info")
-    RequestDto getRequestInfoByDni(@QueryParam("dni") String dni);
+    RequestDto getRequestInfoByDni(
+            @QueryParam("dni")
+            @NotNull(message = "El número de DNI es requerido")
+            @Pattern(regexp = "^\\d{8}$", message = "El número de DNI debe contener 8 dígitos numéricos")
+            String dni);
 }
